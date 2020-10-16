ae80b29ec43654730051fa483d5017ea1fac7e58
gabrielalozano <60881709+gabrielalozano@users.noreply.github.com>
2020-10-16 14:47:01 -0400

    Create RecursionEstrella.cpp

---
+++ FuncionesRecursivas/RecursionEstrella.cpp
@@ -0,0 +1,33 @@
+  
+/*
+Autora: Gabriela Lozano
+Nombre: RecursionEstrella.cpp
+Programa: Crea una funcion recursiva que despliega estrellas dependendiendo del valor que tenga.
+*/
+
+#include <iostream>
+using namespace std;
+
+//Function que despliega valores por fila
+void Despliegue(int num){
+  if(num==0){ // si num ==0, termina la linea y vuelve para atras
+    cout << endl;
+    return;
+  }
+  cout <<"*";
+  Despliegue(num-1); // num-1 para iterar la variable
+}
+
+//Funcion que despliega valores por columnas 
+void Recurs(int num=5){
+  if(num ==0) return; // si num ==0, termina
+Despliegue(num); // despliega primero
+Recurs(num-1); // llama funcion otra vez
+
+}
+
+
+
+int main() {
+  Recurs();
+}
